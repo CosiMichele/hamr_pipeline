652fb3694b6607d27fc2da42cd87afbeab8a3d93
cosimichele <nismichele@gmail.com>
2021-05-25 09:54:36 -0700

    hamr sorghum setup

---
+++ pull_containers_dock.sh
@@ -0,0 +1,8 @@
+docker pull reetututeja/hamr_xi:1.4
+docker pull broadinstitute/gatk3:3.5-0
+docker pull quay.io/biocontainers/samtools:1.10--h2e538c0_3
+docker pull quay.io/biocontainers/picard:2.23.8--0
+docker pull quay.io/biocontainers/hisat2:2.2.0--py27h877ad6c_4
+docker pull quay.io/biocontainers/sra-tools:2.10.9--pl526haddd2b5_0
+docker pull quay.io/biocontainers/salmon:1.5.0--h84f40af_0
+
